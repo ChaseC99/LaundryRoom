40f3ed81c5ec2a3912f556ebf1c6684d30d4d3f0
Chiang Krongapiradee <chiang_sk135@hotmail.com>
2018-02-03 11:03:09 -0800

    Add more routes

--- mysite/laundry/admin.py
+++ mysite/laundry/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Admin, Machine, User
+
+admin.site.register(Admin)
+admin.site.register(Machine)
+admin.site.register(User)
