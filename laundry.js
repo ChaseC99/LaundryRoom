41963797b47c2026b809729207f570833fba69b5
Allyson Yamasaki <allysonyamasaki@dhcp-v111-197.mobile.uci.edu>
2018-02-04 02:41:14 -0800

    timer

--- laundry.js
+++ laundry.js
@@ -91,3 +91,30 @@ function machineSelected(id){
     request.open("GET", url, true);
     request.send();
 }
+
+
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var Minutes = 60 * 1 ,
+        display = document.querySelector('#time');
+    startTimer(Minutes, display);
+};
+       
+
